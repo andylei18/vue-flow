4d4b311693236687a404dad503f749d11f369301
andylei18 <dl375074900@qq.com>
2016-06-21 15:32:27 +0800

    上传文件

--- src/App.vue
+++ src/App.vue
@@ -19,18 +19,21 @@ export default {
     return {
       box:{
         model:[
-            {"id":"1","text":"时间","next":["2","3","11"],"begin":true},
-            {"id":"2","text":"筛选1","next":["4"],"begin":false},
-            {"id":"3","text":"筛选2","next":["5"],"begin":false},
-            {"id":"4","text":"筛选3","next":["6"],"begin":false},
-            {"id":"5","text":"筛选4","next":["7","8"],"begin":false},
-            {"id":"6","text":"筛选5","next":["9"],"begin":false},
-            {"id":"7","text":"筛选6","next":["10","11"],"begin":false},
-            {"id":"8","text":"筛选7","next":["12"],"begin":false},
-            {"id":"9","text":"筛选8","next":["12"],"begin":false},
-            {"id":"10","text":"筛选9","next":["-1"],"begin":false},
+          {"id":"1","text":"时间","next":["2","3","11"],"begin":true},
+          {"id":"2","text":"筛选1","next":["4"],"begin":false}
+            // {"id":"1","text":"时间","next":["2","3","11"],"begin":true},
+            // {"id":"2","text":"筛选1","next":["4"],"begin":false},
+            // {"id":"3","text":"筛选2","next":["5"],"begin":false},
+            // {"id":"4","text":"筛选3","next":["6"],"begin":false},
+            // {"id":"5","text":"筛选4","next":["7","8"],"begin":false},
+            // {"id":"6","text":"筛选5","next":["9"],"begin":false},
+            // {"id":"7","text":"筛选6","next":["10","11"],"begin":false},
+            // {"id":"8","text":"筛选7","next":["12"],"begin":false},
+            // {"id":"9","text":"筛选8","next":["12"],"begin":false},
+            // {"id":"10","text":"筛选9","next":["-1"],"begin":false},
         ],
-        newmodel:[]
+        newmodel:[],
+        width:"50"
       }
     }
   }
--- src/components/box.vue
+++ src/components/box.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="box" v-for="item in box.newmodel"
+  <div class="box" v-for="item in box.model" :id="item.id" :style="{left: item.left,top: item.top}">
       <span>{{item.text}}</span>
   </div>
 </template>
@@ -8,21 +8,52 @@
   export default {
     props: ['box'],
     created (){
-      var self = this,
-          rootID = getRootID(),
-          node = self.$els.flow,
-          myObjectList = self.createObjectList(rootID,0)
+      // const rootid=()=>{
+      //   for(var i in this.box.model){
+      //     if(this.box.model[i].begin){
+      //       console.log(this.box.model[i].id)
+      //       sid = this.box.model[i].id
+      //     }
+      //   }
+      // }
+      //
+      // const node = this.$els.flow
+      // const myObjectList = this.createModel(rootid,0)
 
-      //构建工作流程图的撒打算的撒打算的
-      self.drawDiv(myObjectList,node,0,0,1000)
-
-      function getRootID() {
-        for(var i in self.model){
-          if(self.model[i].begin){
-            rootID = self.model[i].id
-          }
+      //构建工作流程图
+      this.drawDiv()
+    },
+    methods :{
+      //创建新的数组
+      createModel (id,level){
+        console.log(id)
+      },
+      //构建工作流程图
+      drawDiv (){
+        const self = this
+        const flowNode = this.$els.flow
+        const boxModel = self.box.model
+        let firstModel = {
+          id:"1",
+          text:"时间",
+          next:[],
+          begin:true,
+          top:0,
+          left:0
         }
-        return rootID;
+
+        if(boxModel.length==0){
+          boxModel.push(firstModel)
+        }else{
+
+          // DOM 还没有更新
+          this.$nextTick(function () {
+            // DOM 现在更新了
+            // `this` 绑定到当前实例
+            let boxs = document.getElementsByClassName('box')
+            
+          })
+        }
       }
     }
   }
