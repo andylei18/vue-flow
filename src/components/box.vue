eb1f8e87e91e4a8513d9d3c9adb09f14b50527da
andylei18 <dl375074900@qq.com>
2016-06-21 13:01:44 +0800

    vue版的web流程图

---
+++ src/components/box.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="box" v-for="item in box.newmodel"
+      <span>{{item.text}}</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['box'],
+    created (){
+      var self = this,
+          rootID = getRootID(),
+          node = self.$els.flow,
+          myObjectList = self.createObjectList(rootID,0)
+
+      //构建工作流程图的撒打算的撒打算的
+      self.drawDiv(myObjectList,node,0,0,1000)
+
+      function getRootID() {
+        for(var i in self.model){
+          if(self.model[i].begin){
+            rootID = self.model[i].id
+          }
+        }
+        return rootID;
+      }
+    }
+  }
+</script>
